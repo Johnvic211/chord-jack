96c316732aa92d33a02cf885b27b4fea2ed7855c
Johnvic211 <delacruzjohnvic21@gmail.com>
2025-02-28 15:33:46 +0800

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SongPage from "./components/SongPage";
@@ -21,7 +21,7 @@ const App = () => {
 						<Route path="/song-chords/song/:title" element={<SongPage />} />
 						<Route path="/song-chords/playlist" element={<Playlist />} />
 						<Route path="/song-chords/playlist/:playlistName" element={<PlaylistSongManager />} />
-						<Route path="/song-chords/playlist/:playlistName/:title" element={<PlaylistSong/>} />
+						<Route path="/song-chords/playlist/:playlistName/:title" element={<PlaylistSong />} />
 					</Routes>
 				</div>
 				<Footer />
